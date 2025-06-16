0be8206d0a9316bd428f31c72bdc93ddd55917b3
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-06-16 14:25:23 +0530

    update  sevre

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,12 +11,20 @@ app.use(cors());
 app.use(express.json()); //request body parsing
 app.use('/api/users', userRoutes);
 
+
+
+
+
+
+
+
+
 const port = process.env. port||5000;
 // mongodb connection create
 mongoose
      .connect(process.env.MONGO_URI, {useNewUrlParser: true,useUnifiedTopology: true,})
-     .then(() => { console.log('MongoDB connected'); 
-        app.listen(port, () =>console.log(`Server running on port ${port}`));
+     .then(() => { console.log('MongoDB connected 🌿✅'); 
+        app.listen(port, () =>console.log(`Server running on port ${port}📌`));
      }) .catch((error) => console.error('MongoDB connection error:', error));
           
 
