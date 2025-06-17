8ec7387c309abb38064ce336aa58d49756250999
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-06-18 03:47:37 +0530

    create user infomation  controller

---
+++ BACKEND/controllers/ReportIssueController.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const ReportIssue = require('../models/ResportIssue');
+
+const getReportIssues = async (req, res) => {
+    try{ 
+    const ReportIssue = await ReportIssue.find();
+    res.status(200);
+    }
+    catch (error) {
+        res.status(500).json({message: 'Error fetching report issues', error: error.message});
+    }
+};
+
+const  createReportIssue = async (req, res) => {
+    try{
+        const newRoprtIssue = new ReportIssue(req.body);
+        await newRoprtIssue.save();
+        res.status(201)
+        .jason({message: 'Report issue created successfully', data: newRoprtIssue});
+    }
+    catch(err){
+        res.status(500).json({message: 'Error creating report issue', error: err.message});
+    }
+
+};
+
+const dleteReportIssue = async(req,res)=>{
+    try{
+        const {id}= req.params;
+        const dleteReportIssue =  await ReportIssue.findbyIdAndDelete(id);
+        if(!dleteReportIssue){
+            return res.status(404)
+            .json({message: 'Report issue not found'});
+        }
+        res.status(200)
+        .json({message: 'Report issue deleted successfully'});
+       }
+       catch(err){
+        res.status(500)
+        .json({message: 'Error deleting report issue', error: err.message});
+       }
+};
+
+
+const updateReportIssue = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const updateReportIssue = await ReportIssue.findByIdAndUpdate(id, req.body, {new: true});
+        if(!updateReportIssue){
+            res.status(404)
+            .json({message: 'Report issue not found'});
+        }res.status(200)
+        .json({message: 'Report issue updated successfully', data: updateReportIssue});
+    }catch(err){
+        res.status(500)
+        .json({message: 'Error updating report issue', error: err.message});
+    }   
+};
+
+module.exports = {getReportIssues, createReportIssue, dleteReportIssue, updateReportIssue};
