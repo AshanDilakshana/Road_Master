709814372a9a9aa188d540ab6d1f75521501dcf2
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-06-17 20:34:30 +0530

    update user rout

--- BACKEND/routes/usersRoutes.js
+++ BACKEND/routes/usersRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { getUsers, createUser } = require('../controllers/usersController');
+const{logging} = require('../controllers/logging');
 
 // Define routes for user operations
 
 router.get('/admins', getUsers); 
 router.post('/admins_create', createUser);
+router.post('/logging', logging);
+
+
+
+
+
+
 
 module.exports = router;
