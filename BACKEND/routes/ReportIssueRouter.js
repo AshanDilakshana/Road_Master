2fb34141cdfb3854ea66032462eee1810d017d7c
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-07-19 03:25:15 +0530

    update reportissue routes

--- BACKEND/routes/ReportIssueRouter.js
+++ BACKEND/routes/ReportIssueRouter.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {getAllReportIssues,getReportIssueByEmail ,createReportIssue, dleteReportIssue, updateReportIssue,getReportIssueById} = require('../controllers/ReportIssueController');
+const {getAllReportIssues,getReportIssueByEmail ,createReportIssue, dleteReportIssue, updateReportIssue,getReportIssueById, getReportIssuesByProvinceAdmin} = require('../controllers/ReportIssueController');
 const express = require('express');
 const router = express.Router();
 
@@ -9,10 +9,8 @@ router.get('/Getreport', getReportIssueByEmail);
 router.get('/GetreportById/:id', getReportIssueById);
 router.post('/reportIssues_create', createReportIssue);
 router.delete('/reportIssues_delete/:id', dleteReportIssue);
-router.put('/reportIssues_update/:id', updateReportIssue);  
-
-
-
+router.put('/reportIssues_update/:id', updateReportIssue);
+router.get('/GetreportByProvinceAdmin', getReportIssuesByProvinceAdmin);
 
 
 module.exports = router;
